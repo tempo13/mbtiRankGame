d57fd2a7dfefe83b9fbc188bb29e03e0ca8b6c82
Parkjongwho <jongwhop@gmail.com>
2022-05-15 15:45:37 +0900

    Initial Commit

---
+++ mbtiRankGame/mbtiRankGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  mbtiRankGameApp.swift
+//  mbtiRankGame
+//
+//  Created by 박종후 on 2022/05/15.
+//
+
+import SwiftUI
+
+@main
+struct mbtiRankGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
